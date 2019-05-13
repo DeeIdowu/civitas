991c9e527cbaa91c1fd1f1dd8e40cccef53be8b9
Dennis Idowu <deethegreat@Denniss-MacBook-Air.local>
2019-05-13 17:38:12 -0400

    backends

---
+++ config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI"); //value of the default/MongoURI
+
+//to connect to mongoose via asynch await via variable to call in serverjs:
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true
+    }); //return of promise
+
+    console.log("MongoDB connected...");
+  } catch (err) {
+    console.error(err.message);
+    //Exit process with failure
+    process.exit(1);
+  }
+};
+
+//export the connection
+module.exports = connectDB;
