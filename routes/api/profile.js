bb5d1c00356687f7e0e90cf56d11e81c357d11c6
Dennis Idowu <deethegreat@Denniss-MacBook-Air.local>
2019-05-14 17:31:36 -0400

    experiences

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -166,4 +166,68 @@ router.delete("/", auth, async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
+
+//@route  PUT api/profile/experience
+//@desc   PUT Updating/adding profile experience
+//@access Private
+
+router.put(
+  "/experience",
+  [
+    auth,
+    [
+      check("title", "Title is required")
+        .not()
+        .isEmpty(),
+      check("company", "Company is required")
+        .not()
+        .isEmpty(),
+      check("from", "From date is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    //Check for errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    //obtain body data
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    } = req.body;
+    //Creating object with user submission
+    const newExp = {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    };
+
+    try {
+      //Variable for fetching the profile to add experience to
+      const profile = await Profile.findOne({ user: req.user.id });
+      //create array
+      profile.experience.unshift(newExp);
+
+      await profile.save();
+      //response
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
 module.exports = router;
