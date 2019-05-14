da4f79fd7595f7529b33e9b9447081d59ed89146
Dennis Idowu <deethegreat@Denniss-MacBook-Air.local>
2019-05-14 17:13:27 -0400

    curation

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -142,4 +142,28 @@ router.get("/user/:user_id", async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
+
+//@route  DELETE API/Profile
+//@desc   DELETE Profile, Users & Posts
+//@access Private
+router.delete("/", auth, async (req, res) => {
+  try {
+    //@todo - remove users' posts
+    //Remove profile
+    await Profile.findOneAndRemove({
+      user: req.user.id
+    });
+    //Remove User
+    await User.findOneAndRemove({
+      _id: req.user.id
+    });
+    res.json({ msg: "User deleted" });
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === "ObjectId") {
+      return res.status(400).json({ msg: "Profile Not Found" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
 module.exports = router;
