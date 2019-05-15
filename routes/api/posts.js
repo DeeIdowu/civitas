fcf45fcde4dab639d2a00824623949e15c1f3632
Dennis Idowu <deethegreat@Denniss-MacBook-Air.local>
2019-05-15 11:42:49 -0400

    derozan

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,10 +1,52 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator/check");
+const auth = require("../../middleware/auth");
 
-//@route  GET api/posts
-//@desc   This is to test the route
-//@access Public via middleware
+//importation of models:
+const Post = require("../../models/Post");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
 
-router.get("/", (req, res) => res.send("Posts route"));
+//@route  POST api/posts
+//@desc   Create a post
+//@access Private due to login required
+
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("text", "Text is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    //error checking
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      //to retrieve user, name and avatar upon post submission
+      const user = await User.findById(req.user.id).select("-password");
+
+      const newPost = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      };
+      //upon adding post, this is the response
+      const post = await newPost.save();
+      res.json(post);
+    } catch (error) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
