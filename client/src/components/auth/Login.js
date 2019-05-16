891c1b453e9cba9d2bdb89fbc1cb6bf39b8e66f2
Dennis Idowu <deethegreat@Denniss-MacBook-Air.local>
2019-05-16 13:54:08 -0400

    preredux

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,66 @@
-import React from "react";
-
+import React, { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
+/*for user input*/
 const Login = () => {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState({
+    email: "",
+    password: ""
+  });
+
+  /* destructuring */
+  const { name, email, password, password2 } = formData;
+
+  /*onChange handler utilized for event of user input*/
+
+  const onChange = e =>
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  /* For the successful login*/
+  const onSubmit = async e => {
+    e.preventDefault();
+    console.log("Welcome Back!");
+  };
+
+  return (
+    <Fragment>
+      <section className="container">
+        <h1 className="large text-primary">Log in</h1>
+        <p className="lead">
+          <i className="fas fa-user" /> Log into your account
+        </p>
+        <form className="form" onSubmit={e => onSubmit(e)}>
+          <div className="form-group">
+            <input
+              type="email"
+              placeholder="Email Address"
+              name="email"
+              value={email}
+              onChange={e => onChange(e)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              minLength="6"
+              value={password}
+              onChange={e => onChange(e)}
+              required
+            />
+          </div>
+          <input type="submit" className="btn btn-primary" value="Login" />
+        </form>
+        <p className="my-1">
+          Don't have an account?
+          <Link to="/register">Register</Link>
+        </p>
+      </section>
+    </Fragment>
+  );
 };
 
 export default Login;
