4fbbf6c3227b1b0e6932c51e8b39663e3667a71a
Dennis Idowu <deethegreat@Denniss-MacBook-Air.local>
2019-05-16 15:35:46 -0400

    lights

---
+++ client/src/reducers/auth.js
@@ -0,0 +1,39 @@
+import { REGISTER_SUCCESS, REGISTER_FAIL } from "../actions/types";
+
+const initialState = {
+  //get the user token and authentication
+  token: localStorage.getItem("token"),
+  //gets set to true when successful login in conducted
+  isAuthenticated: null,
+  //make sure back-end request is complete, turn to false upon completion
+  loading: true,
+  //user data get places here
+  user: null
+};
+
+//function
+export default function(state = initialState, action) {
+  const { type, payload } = action;
+
+  switch (type) {
+    case REGISTER_SUCCESS:
+      localStorage.setItem("token", payload.token);
+      return {
+        ...state,
+        ...payload,
+        isAuthenticated: true,
+        loading: false
+      };
+    case REGISTER_FAIL:
+      localStorage.removeItem("token");
+      return {
+        ...state,
+        token: null,
+        isAuthenticated: false,
+        loading: false
+      };
+
+    default:
+      return state;
+  }
+}
