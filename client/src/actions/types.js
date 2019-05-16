092094282d72a70101e4f9c1e12567f026188690
Dennis Idowu <deethegreat@Denniss-MacBook-Air.local>
2019-05-16 15:35:27 -0400

    action

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -2,3 +2,7 @@ export const SET_ALERT = "SET_ALERT";
 export const REMOVE_ALERT = "REMOVE_ALERT";
 //If change of action needed, for future references.
 //enables one to have all action types in one place
+
+//for authentication, working with https via axios:
+export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
+export const REGISTER_FAIL = "REGISTER_FAIL";
