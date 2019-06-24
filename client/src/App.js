82d923f883e4b6f89a15ff1ecdc359079cb54238
Dennis Idowu <deethegreat@Denniss-MacBook-Air.local>
2019-06-24 15:50:42 -0400

    wrappingup

--- client/src/App.js
+++ client/src/App.js
@@ -2,26 +2,16 @@ import React, { Fragment, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
-import Login from "./components/auth/Login";
-import Alert from "./components/layout/Alert";
-import Register from "./components/auth/Register";
-//Redux
-import { Provider } from "react-redux"; //middleware to connect react-redux
+import Routes from "./components/routing/Routes";
+
+// Redux
+import { Provider } from "react-redux";
 import store from "./store";
 import { loadUser } from "./actions/auth";
 import setAuthToken from "./utils/setAuthToken";
-import "./App.css";
-import Dashboard from "./components/dashboard/Dashboard";
-import PrivateRoute from "./components/routing/PrivateRoute";
-import CreateProfile from "./components/profile-forms/CreateProfile";
-import AddExperience from "./components/profile-forms/AddExperience";
-import AddEducation from "./components/profile-forms/AddEducation";
-import Profiles from "./components/profiles/Profiles";
-import ProfileItem from "./components/profiles/ProfileItem";
-import Posts from "./components/posts/Posts";
 
-//checking if theres token, then place in global header via src/utils/setAuthToken.js
-//checking via localstorage and doing check on app.js as well
+import "./App.css";
+
 if (localStorage.token) {
   setAuthToken(localStorage.token);
 }
@@ -29,39 +19,17 @@ if (localStorage.token) {
 const App = () => {
   useEffect(() => {
     store.dispatch(loadUser());
-  }, []); //<--to only run once, basically making it a componentDidMount
+  }, []);
+
   return (
     <Provider store={store}>
       <Router>
         <Fragment>
           <Navbar />
-          <Route exact path="/" component={Landing} />
-          <section className="container">
-            <Alert />
-            <Switch>
-              <Route exact path="/register" component={Register} />
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/profiles" component={Profiles} />
-              <Route exact path="/profile-item" component={ProfileItem} />
-              <PrivateRoute exact path="/dashboard" component={Dashboard} />
-              <PrivateRoute
-                exact
-                path="/create-profile"
-                component={CreateProfile}
-              />
-              <PrivateRoute
-                exact
-                path="/add-experience"
-                component={AddExperience}
-              />
-              <PrivateRoute
-                exact
-                path="/add-education"
-                component={AddEducation}
-              />
-              <PrivateRoute exact path="/posts" component={Posts} />
-            </Switch>
-          </section>
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route component={Routes} />
+          </Switch>
         </Fragment>
       </Router>
     </Provider>
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,29 +1,24 @@
 export const SET_ALERT = "SET_ALERT";
 export const REMOVE_ALERT = "REMOVE_ALERT";
-//If change of action needed, for future references.
-//enables one to have all action types in one place
-
-//upon successful login
-export const USER_LOADED = "USER_LOADED";
-//upon unsuccessful login in
-export const AUTH_ERROR = "AUTH_ERROR";
-//for authentication, working with https via axios:
 export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
 export const REGISTER_FAIL = "REGISTER_FAIL";
-//for successful login
+export const USER_LOADED = "USER_LOADED";
+export const AUTH_ERROR = "AUTH_ERROR";
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAIL = "LOGIN_FAIL";
-//for successful logout
 export const LOGOUT = "LOGOUT";
 export const GET_PROFILE = "GET_PROFILE";
-export const PROFILE_ERROR = "PROFILE_ERROR";
-export const CLEAR_PROFILE = "CLEAR_PROFILE";
-//to update:
-export const UPDATE_PROFILE = "UPDATE_PROFILE";
-//DELETE ACCOUNT
-export const ACCOUNT_DELETED = "ACCOUNT_DELETED";
 export const GET_PROFILES = "GET_PROFILES";
 export const GET_REPOS = "GET_REPOS";
-//GET POSTS and POST ERROR;
+export const UPDATE_PROFILE = "UPDATE_PROFILE";
+export const CLEAR_PROFILE = "CLEAR_PROFILE";
+export const PROFILE_ERROR = "PROFIL_ERROR";
+export const ACCOUNT_DELETED = "ACCOUNT_DELETED";
 export const GET_POSTS = "GET_POSTS";
+export const GET_POST = "GET_POST";
 export const POST_ERROR = "POST_ERROR";
+export const UPDATE_LIKES = "UPDATE_LIKES";
+export const DELETE_POST = "DELETE_POST";
+export const ADD_POST = "ADD_POST";
+export const ADD_COMMENT = "ADD_COMMENT";
+export const REMOVE_COMMENT = "REMOVE_COMMENT";
